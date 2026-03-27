cc7608ea6e8a0c443322b66f2ad80caf041bd04e
Lykkeskov <155389868+Lykkeskov@users.noreply.github.com>
2026-03-27 13:32:41 +0100

    Stretch sesnor med CodeCell C3
    Updated sensor pin and threshold for ESP32 compatibility.

--- stretch_sensor.ino
+++ stretch_sensor.ino
@@ -1,11 +1,12 @@
 // testkode til knyttet/åben hånd m. stretch sensor
-const int sensorPin = A0;
+// Nu bruger vi CodeCell C3 i stedet for Arduino UNO. Se tidligere commit for Arduino version: https://github.com/Lykkeskov/DDU-Eksamensprojekt/commit/e09e7cadb47d8a1a6825ed94518d2c57081b8c6f#diff-555a3769563a6809d7aa18cfc66e36a1efeb52e8dcc6c21aba2c164891c4e84f
+const int sensorPin = 2;  // GPIO2
 int sensorValue = 0;
 
-int threshold = 520; // juster efter kalibrering
+int threshold = 2400; // ESP32 har højere opløsning (0–4095)
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -15,9 +16,9 @@ void loop() {
   Serial.println(sensorValue);
 
   if (sensorValue < threshold) {
-    Serial.println("Hånd er KNYTTET");
+    Serial.println("KNYTTET");
   } else {
-    Serial.println("Hånd er ÅBEN");
+    Serial.println("ÅBEN");
   }
 
   delay(200);
