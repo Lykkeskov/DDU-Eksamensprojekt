6caf31578dd2ca81d8d4d08f9d8011a39c906c13
EmilIngiGun <160583896+EmilIngiGun@users.noreply.github.com>
2026-04-04 18:16:40 +0200

    Update Getdata.py
    alle de ændringer der blev glemt at opdatere

--- Getdata.py
+++ Getdata.py
@@ -1,27 +1,51 @@
 from flask import Flask, request
+import logging
 
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)  # Only show errors
 app = Flask(__name__)
 
 @app.route('/data', methods=['POST'])
 def receive_data():
     data = request.get_json()
 
-    proximity = data.get("proximity")
-
     accelerometer = data.get("accelerometer", {})
-    gyro = data.get("gyro", {})
+    motion = data.get("motion", {})
+    linear_acceleration = data.get("lin", {})
 
-    ax = accelerometer.get("x")
-    ay = accelerometer.get("y")
-    az = accelerometer.get("z")
+    ax = float(accelerometer.get("x"))
+    ay = float(accelerometer.get("y"))
+    az = float(accelerometer.get("z"))
 
-    gx = gyro.get("x")
-    gy = gyro.get("y")
-    gz = gyro.get("z")
+    pitch = float(motion.get("pitch")) #Ift hvordan vi vender C3 så skal pitch og roll byttes rundt
+    roll = float(motion.get("roll"))
+    yaw = float(motion.get("yaw"))
 
-    print("Proximity:", proximity)
-    print("Accelerometer:", ax, ay, az)
-    print("Gyro:", gx, gy, gz)
+    linx = float(linear_acceleration.get("lx"))
+    liny = float(linear_acceleration.get("ly"))
+    linz = float(linear_acceleration.get("lz"))
+
+
+
+    truepitch = pitch
+    trueroll = roll
+    trueyaw = yaw
+
+    if truepitch < 0: #Test til at konvertere negative grader i intervallet [-180;180] til intervallet [0:360] bruges måske
+        truepitch = 360 + truepitch
+
+    if trueroll < 0:
+        trueroll = 360 + trueroll
+
+    if trueyaw < 0:
+        trueyaw = 360 + trueyaw
+
+
+    else:
+        print("Accelerometer:", ax, ay, az)
+        print("Linear Acceleration:", linx, liny, linz)
+        print("Motion:", roll, pitch, yaw)
+
 
     return "OK", 200
 
