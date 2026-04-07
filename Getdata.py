07d5f4a2140111edc681b24026f4f0aa9815b907
EmilIngiGun <160583896+EmilIngiGun@users.noreply.github.com>
2026-04-07 11:02:29 +0200

    Update Getdata.py

--- Getdata.py
+++ Getdata.py
@@ -1,12 +1,17 @@
 from flask import Flask, request
 import logging
+import time
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)  # Only show errors
 app = Flask(__name__)
 
+slagAktiv = False
+lastSlagTime = 0  #Tidspunkt for sidste slag
+cooldown = 0.3  #Cooldown mellem slag i sekunder (300 ms)
 @app.route('/data', methods=['POST'])
 def receive_data():
+    global slagAktiv, lastSlagTime, cooldown
     data = request.get_json()
 
     accelerometer = data.get("accelerometer", {})
@@ -25,27 +30,38 @@ def receive_data():
     liny = float(linear_acceleration.get("ly"))
     linz = float(linear_acceleration.get("lz"))
 
+    slag_condition = (              #Sat op så det ser lidt pænere ud og hvis den skal bruges til andre ting
+            abs(ay) > 5 and
+            abs(liny) > 0 and
+            145 <= abs(roll) <= 180 and
+            120 <= abs(pitch) <= 180
+    )
 
+    guard_condition = (abs(liny) < 2 and    #Sat op så det ser lidt pænere ud og hvis den skal bruges til andre ting
+                       -110 < roll < 70 and
+                       5 < abs(ay))
 
-    truepitch = pitch
-    trueroll = roll
-    trueyaw = yaw
-
-    if truepitch < 0: #Test til at konvertere negative grader i intervallet [-180;180] til intervallet [0:360] bruges måske
-        truepitch = 360 + truepitch
-
-    if trueroll < 0:
-        trueroll = 360 + trueroll
-
-    if trueyaw < 0:
-        trueyaw = 360 + trueyaw
-
+    if slag_condition and slagAktiv == False:
+        slagAktiv = True #Bruges til at forhindre dobbelt registrering af slag
+        lastSlagTime = time.time() #Definerer nu tidspunktet for slaget så cooldown kan bruges korrekt
+        print("Slag registreret ",
+              "\n"
+              "\n"
+              "\n")
+    elif guard_condition:
+        print("Guard registreret ",
+              "\n"
+              "\n"
+              "\n")
 
     else:
-        print("Accelerometer:", ax, ay, az)
-        print("Linear Acceleration:", linx, liny, linz)
-        print("Motion:", roll, pitch, yaw)
+        print("Accelerometer: ", ax, ay, az)
+        print("Linear Acceleration: ", linx, liny, linz)
+        print("Motion: ", roll, pitch, yaw)
+        print("Tid: ", time.time()-lastSlagTime)
 
+    if time.time()-lastSlagTime >= cooldown:
+        slagAktiv = False
 
     return "OK", 200
 
