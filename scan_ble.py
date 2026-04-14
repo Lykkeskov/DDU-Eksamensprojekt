6f58392f79a916dba4e678c5afcf9f41b7bd907a
Nikosteenfeldt <160583871+Nikosteenfeldt@users.noreply.github.com>
2026-04-14 13:53:31 +0200

    Add guide for running scan_ble.py
    Added usage instructions for running the script.

--- scan_ble.py
+++ scan_ble.py
@@ -7,3 +7,10 @@ async def main():
         print(f"Name: {d.name}, Address: {d.address}")
 
 asyncio.run(main())
+
+#GUIDE: Sådan gør jeg; Læg denne fil ind i din desktop på computeren og åben terminal. 
+#Når terminal er åben run: 'cd desktop' og dette åbener op ind til ens fil. 
+#Derefter skrev: 'python scan_ble.py' VIGTIGT man ikke har skiftet navnet på filen. 
+#Det kan tage noget tid, men ellers finder din computer alle bluetooth devices
+#Led efter; 'Codecell_Right' og 'Codecell_Left' og det er disse addreser man skal bruge
+
