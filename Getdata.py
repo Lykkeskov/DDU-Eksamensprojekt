00466a3e967b1a619918b00f3b3480d9c53ba347
Lykkeskov <h.lykkeskov@gmail.com>
2026-04-22 10:27:30 +0200

    Har kombineret flex og virate med slag detection. Stadig lidt buggy, men på vej.

--- Getdata.py
+++ Getdata.py
@@ -1,25 +1,45 @@
 import asyncio
-from bleak import BleakClient
+from bleak import BleakClient, BleakScanner
 import time
+import json
+
+ble_clients = {}
+COMMAND_UUID = "dcba4321-dcba-4321-dcba-4321fedcba98"
+
 
 Devices = {
     "CodeCell_Right": "28:37:2F:C7:C5:D2",
     "CodeCell_Left": "98:3D:AE:38:32:0E"
 }
 
+# Forbind kun til devices der er tændt (undgå crash hvis de nene ikke er tændt)
+async def discover_devices():
+    found = await BleakScanner.discover(timeout=5.0)
+
+    available = {}
+
+    for d in found:
+        if d.name in Devices:
+            available[d.name] = d.address
+
+    return available
 
 CHAR_UUID = "abcd5678-abcd-5678-abcd-56789abcdef0"
 
+damage_flag = False
 
 slagAktiv = False
 lastSlagTime = 0  #Tidspunkt for sidste slag
 cooldown = 1  #Cooldown mellem slag i sekunder (300 ms)
 
+async def trigger_vibration_all():
+    for name in list(ble_clients.keys()):
+        await vibrate_device(name)
 
 def make_handler(name):
     def notification_handler(sender, data):
         global slagAktiv, lastSlagTime, cooldown
-
+        '''
         values = data.decode().split(",")
 
         xAcceleratinon = float(values[0]) #Acceleratinoerne hen af de forskellige akser
@@ -33,6 +53,32 @@ def make_handler(name):
         xLinAcceleration = float(values[6]) #Den linæer acceleration hvilket trækker tyngdekraften
         yLinAcceleration = float(values[7])
         zLinAcceleration = float(values[8])
+        '''
+
+        try:
+            decoded = data.decode()
+            parsed = json.loads(decoded)
+        except Exception as e:
+            print("JSON parse error:", e)
+            print("Raw data:", data)
+            return
+
+
+        # Extract values
+        flexValue = parsed["flex"]
+        isFist = parsed["fist"]
+
+        xAcceleration = parsed["ax"]
+        yAcceleration = parsed["ay"]
+        zAcceleration = parsed["az"]
+
+        roll = parsed["rx"]
+        pitch = parsed["ry"]
+        yaw = parsed["rz"]
+
+        xLinAcceleration = parsed["lx"]
+        yLinAcceleration = parsed["ly"]
+        zLinAcceleration = parsed["lz"]
 
         slag_condition = (  # Sat op så det ser lidt pænere ud og hvis den skal bruges til andre ting
                 abs(yAcceleration) > 6 and
@@ -45,13 +91,12 @@ def make_handler(name):
                            #-110 < roll < -70 and
                            )
 
-        if slag_condition and slagAktiv == False:
+        if slag_condition and isFist and slagAktiv == False:
             slagAktiv = True  # Bruges til at forhindre dobbelt registrering af slag
             lastSlagTime = time.time()  # Definerer nu tidspunktet for slaget så cooldown kan bruges korrekt
-            print(f"\n[{name}] ", "Slag registreret"
-                  "\n"
-                  "\n"
-                  "\n")
+            print(f"\n[{name}] Slag registreret\n")
+
+            asyncio.create_task(trigger_vibration_all())
 
         elif guard_condition and slagAktiv == False:
             print(f"\n[{name}] ", "Guard registreret"
@@ -60,7 +105,7 @@ def make_handler(name):
                   "\n")
         else:
             print(f"\n[{name}]")
-            print("Accel:", "x", xAcceleratinon, "y", yAcceleration, "z", zAcceleration)
+            print("Accel:", "x", xAcceleration, "y", yAcceleration, "z", zAcceleration)
             print("Rot  :", "Roll", roll, "Pitch",pitch, "Yaw",yaw)
             print("Lin  :","x" ,xLinAcceleration, "y", yLinAcceleration, "z",zLinAcceleration)
 
@@ -72,21 +117,42 @@ def make_handler(name):
     return notification_handler
 
 
+async def vibrate_device(name):
+    if name in ble_clients:
+        client = ble_clients[name]
+
+        if client.is_connected:
+            try:
+                await client.write_gatt_char(COMMAND_UUID, b"V")
+                print(f"{name} VIBRATE")
+            except Exception as e:
+                print("BLE error:", e)
+
 async def connect_device(name, address):
-    async with BleakClient(address) as client:
+    try:
+        client = BleakClient(address)
+        await client.connect(timeout=10.0)
+
         print(f"{name} connected")
+
+        ble_clients[name] = client
 
         await client.start_notify(CHAR_UUID, make_handler(name))
 
         while True:
             await asyncio.sleep(1)
 
+    except Exception as e:
+        print(f"{name} FAILED:", e)
+
 
 async def main():
     tasks = []
+    available_devices = await discover_devices()
 
-    for name, address in Devices.items():
+    for name, address in available_devices.items():
         tasks.append(connect_device(name, address))
+
     await asyncio.gather(*tasks)
 
 
